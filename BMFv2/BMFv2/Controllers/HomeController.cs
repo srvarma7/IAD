6f309c3b2e5b2433e02f6729da074837f5ee3a7c
srvarma7 <srvarma7@gmail.com>
2019-10-13 13:09:06 +1100

    First commit

---
+++ BMFv2/BMFv2/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BMFv2.Controllers
+{
+    [RequireHttps]
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult OnlineUsers()
+        {
+            ViewBag.Message = "Your online users page.";
+
+            return View();
+        }
+    }
+}
---
+++ BMFv2/BMFv2/Utils/EmailSender.cs
@@ -0,0 +1,27 @@
+﻿using SendGrid;
+using SendGrid.Helpers.Mail;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace BMFv2.Utils
+{
+    public class EmailSender
+    {
+
+        private const String API_KEY = "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";
+
+        public void Send(String toEmail, String subject, String contents)
+        {
+            var client = new SendGridClient(API_KEY);
+            var from = new EmailAddress("noreply@bookmyflight.com", "BookMyFlight");
+            var to = new EmailAddress(toEmail, "srvarma7@gmail.com");
+            var plainTextContent = contents;
+            var htmlContent = "<p>" + contents + "</p>";
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+            var response = client.SendEmailAsync(msg);
+        }
+    }
+}
