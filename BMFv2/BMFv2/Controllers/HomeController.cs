075404a1e3fea594323bfba0bbf14e9bf4d311cf
srvarma7 <srvarma7@gmail.com>
2019-10-13 22:50:24 +1100

    Generates PDF from URL

--- BMFv2/BMFv2/Controllers/HomeController.cs
+++ BMFv2/BMFv2/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using IronPdf;
 
 namespace BMFv2.Controllers
 {
@@ -33,6 +34,23 @@ namespace BMFv2.Controllers
             ViewBag.Message = "Your online users page.";
 
             return View();
+        }
+
+        public ActionResult invoice()
+        {
+            return View();
+        }
+        public ActionResult GeneratePDF()
+        {
+            var Renderer = new IronPdf.HtmlToPdf();
+            //var PDF = Renderer.RenderHTMLFileAsPdf("https://localhost:44335/Home/About.cshtml");
+            var PDF = Renderer.RenderUrlAsPdf("https://localhost:44335/Home/invoice");
+            //var PDF = Renderer.RenderHTMLFileAsPdf("C:/Users/Raghu/Desktop/BMF/IAD/BMFv2/BMFv2/Views/Home/Index.cshtml");
+            var OutputPath = "C:/Users/Raghu/Desktop/Home.pdf";
+            PDF.SaveAs(OutputPath);
+
+            return RedirectToAction("Index");
+            //return View();
         }
     }
 }
